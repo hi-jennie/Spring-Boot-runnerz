1fbb4a8b2e9cf53ed1d7d7d497b8477021a3db4b
hi-jennie <jennie.wang2024@gmail.com>
2024-10-09 15:30:49 +0800

    add other method using JDBC

--- src/main/java/dev/danvega/runnerz/Application.java
+++ src/main/java/dev/danvega/runnerz/Application.java
@@ -2,6 +2,7 @@ package dev.danvega.runnerz;
 
 import dev.danvega.runnerz.run.Location;
 import dev.danvega.runnerz.run.Run;
+import dev.danvega.runnerz.run.RunRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -22,14 +23,14 @@ public class Application {
 	}
 
 	@Bean
-	CommandLineRunner runner(){
+	CommandLineRunner runner(RunRepository runRepository){
 		return args -> {
 			Run run = new Run(
 					1, "First Run",
 					LocalDateTime.now(),
 					LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5,
 					Location.OUTDOOR);
-			log.info("Run: "+run);
+			runRepository.create(run);
 		};
 	}
 
--- src/main/java/dev/danvega/runnerz/run/RunRepository.java
+++ src/main/java/dev/danvega/runnerz/run/RunRepository.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class RunRepository{
@@ -20,6 +22,55 @@ public class RunRepository{
     public List<Run> findAll(){
         return jdbcClient.sql("select * from run")
                 .query(Run.class)// map to Run class
+                .list();
+    }
+
+    public Optional<Run> findById(Integer id){
+        return jdbcClient.sql("select id, title, started_on, completed_on, miles, location FRON run where id = :id")
+                .param("id", id) // parameter Binding
+                .query(Run.class)
+                .optional();
+
+    }
+
+    public void create(Run run){
+        var updated = jdbcClient.sql("INSERT INTO run (id, title, started_on, completed_on, miles, location) values (?, ?, ?, ?, ?, ?)")
+                .params(List.of(run.id(), run.title(), run.startedOn(), run.completedOn(), run.miles(), run.location().toString()))
+                .update();
+
+        Assert.state(updated ==1, "Failed to create run"+run.title());
+        // 	•	updated == 1 作为条件：如果这个条件为真，程序就会继续执行。
+        //	•	如果条件为假：程序会抛出异常并显示错误信息，相当于执行了一个“否则”的操作。
+    }
+
+    public void update(Run run, Integer id){
+        var updated = jdbcClient.sql("UPDATE run SET title = ?, started_on = ?, completed_on = ?, miles = ?, location = ? WHERE id = ?")
+                .params(List.of(run.title(), run.startedOn(), run.completedOn(), run.miles(), run.location().toString(), id))
+                .update();
+
+        Assert.state(updated == 1, "Failed to update run"+run.title());
+    }
+
+    public void delete(Integer id){
+        var updated = jdbcClient.sql("DELETE FROM run WHERE id = ?")
+                .param(id)
+                .update();
+
+        Assert.state(updated == 1, "Failed to delete run"+id);
+    }
+
+    public int count(){
+        return jdbcClient.sql("select * from run").query().listOfRows().size();
+    }
+
+    public void saveAll(List<Run> runs){
+        runs.stream().forEach(this::create);
+    }
+
+    public List<Run> findByLocation(String location){
+        return jdbcClient.sql("select * from run where location = ?")
+                .param(location )
+                .query(Run.class)
                 .list();
     }
 }
